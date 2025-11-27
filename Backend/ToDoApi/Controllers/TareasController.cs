7574c95186dacd5f28d961c8edf8727464f9f3c6
Rubén Chávez <ruben.chvz17@gmail.com>
2025-11-27 16:29:34 -0700

    Backend: Conectado a SQL y Endpoint GET funcionando

---
+++ Backend/ToDoApi/Controllers/TareasController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using ToDoApi.Models;
+
+namespace ToDoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareasController : ControllerBase
+    {
+        private readonly string cadenaConexion;
+
+        public TareasController(IConfiguration config)
+        {
+            cadenaConexion = config.GetConnectionString("CadenaSQL");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTareas()
+        {
+            var lista = new List<Tarea>();
+
+            using (var conexion = new SqlConnection(cadenaConexion))
+            {
+                await conexion.OpenAsync();
+                
+                using (var comando = new SqlCommand("SELECT * FROM Tareas", conexion))
+                {
+                    using (var lector = await comando.ExecuteReaderAsync())
+                    {
+                        while (await lector.ReadAsync())
+                        {
+                            var tarea = new Tarea();
+                            tarea.Id = Convert.ToInt32(lector["Id"]);
+                            tarea.Titulo = lector["Titulo"].ToString();
+                            tarea.Completada = Convert.ToBoolean(lector["Completada"]);
+                            tarea.FechaCreacion = Convert.ToDateTime(lector["FechaCreacion"]);
+
+                            lista.Add(tarea);
+                        }
+                    }
+                }
+            }
+
+            return Ok(lista);
+        }
+    }
+}
---
+++ Backend/ToDoApi/Models/Tarea.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoApi.Models
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public bool Completada { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
